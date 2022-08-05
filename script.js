aab77062ed4b98dd356d2fcf036ea5f2def42b0a
Donald Riedl <riedldsr@gmail.com>
2022-08-04 21:31:48 -0600

    Added background blur

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function showMenu() {
 	let menu = document.querySelector("#menu");
 	let menuIcon = document.querySelector("#menuIcon");
+	let main = document.querySelector("main");
 
 	if(menu.classList.contains("displayed")) {
 		menu.classList.remove("displayed");
 		menuIcon.classList.remove("closeIcon");
+		main.classList.remove("blur");
 	} else {
 		menu.classList.add("displayed");
 		menuIcon.classList.add("closeIcon");
+		main.classList.add("blur");
 	}
 }

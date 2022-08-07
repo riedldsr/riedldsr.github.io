85ecba6fb48735b3f3e9cfc31bdc8b56c9480367
Donald Riedl <riedldsr@gmail.com>
2022-08-06 22:27:02 -0600

    Changed menu, added full screen image viewer

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-function showMenu() {
+function toggleOverlay() {
 	let menu = document.querySelector("#menu");
+	let menuList = document.querySelector("#menu ul");
 	let menuIcon = document.querySelector("#menuIcon");
 	let main = document.querySelector("main");
+	let overlay = document.querySelector("#overlay");
+	let image = document.querySelector("#fullScreenImage");
 
-	if(menu.classList.contains("displayed")) {
+	if(overlay.classList.contains("displayed")) {
 		menu.classList.remove("displayed");
 		menuIcon.classList.remove("closeIcon");
 		main.classList.remove("blur");
+		overlay.classList.remove("displayed");
+		menuList.classList.remove("displayed");
+		if(image) {
+			image.classList.remove("displayed");
+			setTimeout(() => { image.src = ""; }, 500);
+		}
 	} else {
 		menu.classList.add("displayed");
 		menuIcon.classList.add("closeIcon");
 		main.classList.add("blur");
+		overlay.classList.add("displayed");
+		menuList.classList.add("displayed");
 	}
+}
+
+let portfolioImages = document.querySelectorAll(".imageContainer>img");
+for(let i = 0; i < portfolioImages.length; i++) {
+	portfolioImages[i].addEventListener('click', function(e) {
+		let overlay = document.querySelector("#overlay");
+		let main = document.querySelector("main");
+		let image = document.querySelector("#fullScreenImage");
+
+		overlay.classList.add("displayed");
+		main.classList.add("blur");
+		image.classList.add("displayed");
+		image.src = e.target.src;
+	});
 }
 
 function sendEmail() {
